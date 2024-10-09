19cec2d1212da5f4110278fcdf23a9c7edbf1c3f
AkaakuHub <105225777+github-actions@users.noreply.github.com>
2024-10-10 00:50:45 +0900

    chore: APIガイド

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { RunMode } from '@/types';
-
 interface FooterProps {
   isTurtle: boolean;
 }
 
 export default function Footer({ isTurtle }: FooterProps) {
   const NEXT_PUBLIC_GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL || '';
-
+  const hrefURLs = [window.location.pathname, `${window.location.pathname}?turtle`];
   return (
-    <div className="footer-links">
-      <a href={NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noopener noreferrer">
-        GitHub
-      </a>
-      {/* <span>
+    <div>
+      <div className="footer-links">
+        <a href={NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        {/* <span>
         ・
       </span> */}
-      {/* {
+        {/* {
         runMode === "server" ? (
           <a href="/">
             戻る
@@ -28,20 +27,24 @@ export default function Footer({ isTurtle }: FooterProps) {
           </a>
         )
       } */}
-      <span>
-        ・
-      </span>
-      {
-        isTurtle ? (
-          <a href={window.location.pathname}>
-            🏠️
-          </a>
-        ) : (
-          <a href={`${window.location.pathname}?turtle`}>
-            🐢
-          </a>
-        )
-      }
+        <span>
+          ・
+        </span>
+        {
+          isTurtle ? (
+            <a href={hrefURLs[0]}>
+              🏠️
+            </a>
+          ) : (
+            <a href={hrefURLs[1]}>
+              🐢
+            </a>
+          )
+        }
+      </div>
+      <div>
+        Try this API by POST &#123; image: &#091;base64&#093; &#125; to <a href={isTurtle ? hrefURLs[1] : hrefURLs[0]}>/api/run</a>
+      </div>
     </div>
   )
 }
